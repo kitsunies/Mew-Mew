e1a4512ad0b2f469bf896b948e0b4ef09d08de67
Kitsun <kitsunies@gmail.com>
2020-10-20 07:37:08 +1100

    Removed unnessarary stuff

--- core/events.lua
+++ core/events.lua
@@ -40,75 +40,10 @@ function events.messageCreate(client, msg)
         return client.owner:sendf('%s said: %s', msg.author.mentionString, msg.content)
     end
 
-    --[[
-    if modules.experience then
-        modules.experience(msg)
-    end
-    --]]
-    if modules.commands then
-        modules.commands.create(msg)
-    end
-
     if modules.manual then
         modules.manual(msg)
     end
 
 end
-
-
-function events.messageDelete(client, msg)
-
-    if modules.commands then
-        modules.commands.delete(msg)
-    end
-
-	if modules.undelete then
-		modules.undelete(msg)
-	end
-
-end
-
---[[
-function events.messageUpdate(...)
-    print('messageUpdate: Nothing to connect to!')
-end
-
-
-function events.messageDelete(...)
-    print('messageDelete: Nothing to connect to!')
-end
-
-
-function events.messageUpdateUncached(...)
-    print('messageUpdateUncached: Nothing to connect to!')
-end
-
-
-function events.messageDeleteUncached(...)
-    print('messageDeleteUncached: Nothing to connect to!')
-end
-
-
---|| Reactions
-
-function events.reactionAdd(...)
-    print('reactionAdd: Nothing to connect to!')
-end
-
-
-function events.reactionRemove(...)
-    print('reactionRemove: Nothing to connect to!')
-end
-
-
-function events.reactionAddUncached(...)
-    print('reactionAddUncached: Nothing to connect to!')
-end
-
-
-function events.reactionRemoveUncached(...)
-    print('reactionRemoveUncached: Nothing to connect to!')
-end
-]]
 
 return events
