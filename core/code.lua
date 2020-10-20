1f29612585268315068a854727960fcd618b399b
Kitsun <kitsunies@gmail.com>
2020-10-20 11:23:27 +1100

    Added public eval

---
+++ core/code.lua
@@ -0,0 +1,50 @@
+local sandbox = loader.load('sandbox')
+local pp = require("pretty-print")
+
+local prefix = loader.constants.settings.prefix 
+
+local function printLine(...)
+    local ret = {}
+    for i = 1, select('#', ...) do
+        local arg = tostring(select(i, ...))
+        table.insert(ret, arg)
+    end
+    return table.concat(ret, '\t')
+end
+
+local function prettyLine(...)
+    local ret = {}
+    for i = 1, select('#', ...) do
+        local arg = pp.strip(pp.dump(select(i, ...)))
+        table.insert(ret, arg)
+    end
+    return table.concat(ret, '\t')
+end
+
+local env = {}
+
+return function(msg)
+
+    local arg = msg.content:match(prefix..'code%s+(.+)')
+  
+    if not arg then return end
+    
+    arg = arg:gsub('```lua\n?', ''):gsub('```\n?', '')
+    
+    local lines = {}
+
+    env.print = function(...) table.insert(lines, printLine(...)) end
+    env.p = function(...) table.insert(lines, prettyLine(...)) end
+    
+    local success, runtimeError = pcall(sandbox.run, arg, {env = env})
+    local cline = runtimeError:gsub('^.-%s', '') or table.concat(lines, '\n')
+    local output = success and 'success' or 'error'
+
+    return msg:reply{
+        embed = {
+            title = 'Evaluation',
+            description = string.format('Output [%s]\n```lua\n%s```', output, cline),
+            color = 0x00007c
+        }
+    }
+end
--- core/manual.lua
+++ core/manual.lua
@@ -1,5 +1,5 @@
 local url = 'https://www.lua.org/manual/%s/manual.html'
-local pretag = '<hr><h3><a name='
+local pretag = '<hr><h3><a name="'
 local prefix = loader.constants.settings.prefix
 require('discordia').extensions()
 
@@ -31,7 +31,7 @@ local function sub(sub)
         ['<pre>'] = '```lua', ['</pre>'] = '```\n'
     })
     --> Unique Subs
-    sub = sub:gsub('<a href='.-'>', '')
+    sub = sub:gsub('<a href=".-">', '')
     sub = sub:gsub('\n\n+', '=\n\n=')
     sub = sub:gsub('(%S)\n(%S)', '%1 %2')
     return sub
@@ -39,9 +39,9 @@ end
 
 return function(msg)
 
-    if not loader.constants.manual['5.4'] then msg:reply('Lua manuals are loading...') return end
     if msg.content:sub(1, #prefix+3) ~= prefix..'man' then return end
     if msg.author.id == msg.client.user.id then return end
+    if not loader.constants.manual['5.4'] then msg:reply('Lua manuals are loading...') return end
 
     local query = msg.content:split(' ')[2]
     local version
@@ -63,27 +63,57 @@ return function(msg)
             sub(data:match(pretag..'[pdf%-]+'..query..'"><code>.-</code></a></h3>(.-)<hr>'))
         }
     end)
+
+    local keyPairs = { 
+        'Library', 
+        'C API', 
+        'C Aux' 
+    }
+
+    local switch = {
+        ['pdf-'] = 'Library',
+        ['lua_'] = 'C API',
+        ['luaL_'] = 'C Aux'
+    }
+
     local title, body
     if suc then
         title, body = unpack(ret)
     else
         local results = {}
+        local temps = {
+            ['Library'] = {},
+            ['C API'] = {},
+            ['C Aux'] = {}
+        }
         local count = 0
         for str in data:gmatch(pretag..'(%S-'..query..'%S-)">') do
             for match in data:gmatch('<h3><a name="(%S-'..query..'%S-)">') do
                 local str = ('[`%s`](%s%s%s)'):format(match:gsub('pdf%-', ''), url, '#', match)
+                if match:gsub('pdf%-', '') ~= query then
+                    for k in pairs(switch) do
+                        if str:find(k) then
+                            temps[switch[k]][#temps[switch[k]]+1] = str
+                        end    
+                    end
+                end
+            end
+        end
+        for _, k in ipairs(keyPairs) do
+            for _, v in ipairs(temps[k]) do
                 local found = false
-                for _, v in pairs(results) do
-                    if v == str then
+                for _, r in pairs(results) do
+                    if v == r then
                         found = true
                     end
                 end
-                if match:gsub('pdf%-', '') ~= query and not found and count < 10 then
-                    results[#results+1] = str
+                if v ~= query and count < 10 and not found then 
+                    results[#results+1] = v
                     count = count + 1
                 end
             end
         end
+        
         return msg:reply {
             embed = {
                 title = 'Lua '..version..' Manual',
@@ -116,18 +146,6 @@ return function(msg)
         ['Library'] = {},
         ['C API'] = {},
         ['C Aux'] = {}
-    }
-
-    local switch = {
-        ['pdf-'] = 'Library',
-        ['lua_'] = 'C API',
-        ['luaL_'] = 'C Aux'
-    }
-
-    local keyPairs = { 
-        'Library', 
-        'C API', 
-        'C Aux' 
     }
 
     for lib, post in data:gmatch(pretag..'(%S-)'..query..'(%S-)">') do
