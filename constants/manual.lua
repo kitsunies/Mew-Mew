08d372254895cb67ba4bbdd68992e0392222ef0f
Kitsun <kitsunies@gmail.com>
2020-10-20 19:47:57 +1100

    Added loading state

--- constants/manual.lua
+++ constants/manual.lua
@@ -1,7 +1,7 @@
 local manual = 'https://www.lua.org/manual/%s/manual.html'
 local http = require 'coro-http'
 
-local manuals = {}
+local manuals = {state = false}
 
 coroutine.wrap(function()
     for i = 5.1, 5.4, 0.1 do
@@ -9,6 +9,7 @@ coroutine.wrap(function()
         local res, data = http.request('GET', manual:format(i))
         manuals[i] = data and tostring(data)
     end
+    manuals.state = true
 end)()
 
 return manuals
