423143363faae373230323fba869f250e4f5cabf
Kitsun <kitsunies@gmail.com>
2020-10-20 19:47:23 +1100

    Added more flexibility

--- modules/manual.lua
+++ modules/manual.lua
@@ -39,21 +39,17 @@ end
 
 return function(msg)
 
+    local version, query = msg.content:match(prefix..'man([0-9]?%.?[0-9]?)%s(.*)')
     if msg.content:sub(1, #prefix+3) ~= prefix..'man' then return end
     if msg.author.id == msg.client.user.id then return end
-    if not loader.constants.manual['5.4'] then msg:reply('Lua manuals are loading...') return end
+    if not loader.constants.manual.state then msg:reply('Lua manuals are loading...') return end
+    if not version or not query then return end
 
-    local query = msg.content:split(' ')[2]
-    local version
-
-    if msg.content:sub(6, 6) ~= '.' then 
-        version = msg.content:sub(5, 5) .. '.' .. msg.content:sub(6, 6)
-    else
-        version = msg.content:sub(5, 7)
+    if #version == 0 then 
+        version = '5.1'
+    elseif #version < 3 then 
+        version = version:gsub('(.)(.)', '%1.%2')
     end
-
-    if not query then return end
-    if not version then return end
 
     local url = url:format(version)
     local data = loader.constants.manual[version]
@@ -77,9 +73,13 @@ return function(msg)
     }
 
     local title, body
+
     if suc then
+
         title, body = unpack(ret)
+        
     else
+
         local results = {}
         local temps = {
             ['Library'] = {},
@@ -87,6 +87,7 @@ return function(msg)
             ['C Aux'] = {}
         }
         local count = 0
+
         for str in data:gmatch(pretag..'(%S-'..query..'%S-)">') do
             for match in data:gmatch('<h3><a name="(%S-'..query..'%S-)">') do
                 local str = ('[`%s`](%s%s%s)'):format(match:gsub('pdf%-', ''), url, '#', match)
@@ -99,6 +100,7 @@ return function(msg)
                 end
             end
         end
+        
         for _, k in ipairs(keyPairs) do
             for _, v in ipairs(temps[k]) do
                 local found = false
@@ -114,29 +116,29 @@ return function(msg)
             end
         end
         
+        
         return msg:reply {
             embed = {
                 title = 'Lua '..version..' Manual',
                 url = url,
                 description = 'No results for: `'..query..'`',
-                fields = {
-                    {
-                        name = 'Did you mean:', 
-                        value = table.concat(results, ', '),
-                        inline = false
-                    }
-                },
+                fields = #results > 0 and {{
+                    name = 'Did you mean:', 
+                    value = table.concat(results, ', '),
+                    inline = false
+                }} or {},
                 color = 0x00007c
             }    
         }
-    end
-    local description = ('----\n\n[`%s`](%s#pdf-%s)'):format(title, url, query)
 
+    end
+
+    local description = ('----\n\n[`%s`](%s#pdf-%s)'):format(title, url, query)
 
     local fields = {}
     for paragraph in body:gmatch('\n=(.-)=\n') do
         fields[#fields+1] = {
-            name = '​', -- zero length white space >w<
+            name = '​', -- zero length space
             value = paragraph,
             inline = false
         }
@@ -150,16 +152,19 @@ return function(msg)
 
     for lib, post in data:gmatch(pretag..'(%S-)'..query..'(%S-)">') do
         local sub
+
         for k in pairs(switch) do
             if lib:find(k) then
                 sub = k
             end    
         end
+
         local pre = lib:sub(#sub+1, #lib)
         local fol = switch[sub]
         pre = pre or ''
         post = post or ''
         sub = sub == 'pdf-' and 'pdf%-' or sub
+
         for match in data:gmatch('"('..sub..pre..query..post..')">') do
             if match:gsub('pdf%-', '') ~= query then
                 match = ('[`%s`](%s%s%s)'):format(match:gsub('pdf%-', ''), url, '#', match)
